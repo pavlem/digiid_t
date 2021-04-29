5faa23bcaf4a7e42885f33e7eaf6b3f6062c609d
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2021-04-29 14:21:24 +0200

    Initial setup

---
+++ DigiIdentity_test/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DigiIdentity_test
+//
+//  Created by Pavle Mijatovic on 29.4.21..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
